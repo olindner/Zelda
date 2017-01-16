4354191e3727dbd8b1f1bbcc4e3dbeb6121596db
lilhuang <lilhuang@umich.edu>
2017-01-16 01:52:56 -0500

    worked a bit on Link's weapon usage

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public enum EntityState {NORMAL, ATTACKING};
 
 public class PlayerController : MonoBehaviour {
 
+	//public int frame = 0; //for debugging
+
     /* Inspector Tunables */
     public float PlayerMovementVelocity;
 	public int showDamageForFrames = 2;
@@ -17,6 +19,9 @@ public class PlayerController : MonoBehaviour {
 	public int remainingDamageFrames = 0;
 	public int remainingDamageFlashes = 0;
 	public Color[] originalColors;
+
+	public int num_cooldown_frames = 0;
+	public float damage_hopback_vel = 2.65f; //how fast Link hops back when damaged in comparison to velocity before
 
 	public int num_rupees = 0;
 	public float num_hearts;
@@ -35,6 +40,9 @@ public class PlayerController : MonoBehaviour {
 	public Sprite[] link_right_damage;
 	public Sprite[] link_left_damage;
 
+	public Sprite[] link_attack; //first element down, then left, then up, then right
+	//public Sprite[] swords; //first element down, then left, then up, then right
+
 	StateMachine animation_state_machine;
 	StateMachine control_state_machine;
 
@@ -42,6 +50,9 @@ public class PlayerController : MonoBehaviour {
 	public Direction current_direction = Direction.SOUTH;
 
 	public GameObject selected_weapon_prefab;
+	static public Dictionary<WeaponType, WeaponDefinition> W_DEFS;
+	public WeaponDefinition[] weaponDefinitions;
+	//public Weapon current_weapon;
 
 	public static PlayerController instance;
 
@@ -49,6 +60,11 @@ public class PlayerController : MonoBehaviour {
     void Start () {
         rb = GetComponent<Rigidbody>();
         instance = this;
+
+		W_DEFS = new Dictionary<WeaponType, WeaponDefinition> ();
+		foreach (WeaponDefinition def in weaponDefinitions) {
+			W_DEFS [def.type] = def;
+		}
 
 		materials = Utils.GetAllMaterials (gameObject);
 		originalColors = new Color[materials.Length];
@@ -63,22 +79,41 @@ public class PlayerController : MonoBehaviour {
     
     // Update is called once per frame
     void Update () {
+		//frame++;
+		if (current_state == EntityState.ATTACKING)
+			current_state = EntityState.NORMAL;
+		
+
         ProcessMovement();
         ProcessAttacks();
 		animation_state_machine.Update ();
 
 		if (remainingDamageFlashes > 0) {
+			//print ("frame number: " + frame);
+			//print (remainingDamageFlashes + " damage flashes left");
 			if (remainingDamageFrames > 0) {
+				//print (remainingDamageFrames + " damage frames left");
 				remainingDamageFrames--;
-				if (remainingDamageFrames == 0) {
+				if (remainingDamageFrames == showDamageForFrames / 2) {
+					//print ("no damage frames left!");
 					UnshowDamage ();
 				}
 			} else {
+				//print ("decreasing damage flashes left");
 				remainingDamageFlashes--;
 				ShowDamage (remainingDamageFlashes);
 			}
 		} else {
+			//print ("no damage flashes left!");
 			UnshowDamage ();
+		}
+
+		if (num_cooldown_frames > 0) {
+			num_cooldown_frames--;
+			if (num_cooldown_frames == 0) {
+				GetComponent<Rigidbody>().velocity = Vector3.zero;
+				//print ("cooldown over!!!!");
+			}
 		}
     }
 
@@ -88,49 +123,51 @@ public class PlayerController : MonoBehaviour {
 		float grid_offset_y = 0.0f;
         Vector3 desired_velocity = Vector3.zero;
 
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			desired_velocity = new Vector3 (0, 1, 0);
-			float temp;
-			if (rb.position.x % 0.5f < 0.25) {
-				temp = Mathf.Floor (rb.position.x / 0.5f) * 0.5f;
-			} else {
-				temp = Mathf.Ceil (rb.position.x / 0.5f) * 0.5f;
+		if (num_cooldown_frames == 0) {
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				desired_velocity = new Vector3 (0, 1, 0);
+				float temp;
+				if (rb.position.x % 0.5f < 0.25) {
+					temp = Mathf.Floor (rb.position.x / 0.5f) * 0.5f;
+				} else {
+					temp = Mathf.Ceil (rb.position.x / 0.5f) * 0.5f;
+				}
+				Vector3 newpos = new Vector3 (temp, rb.transform.position.y, 0);
+				rb.transform.position = newpos;
+			} else if (Input.GetKey (KeyCode.LeftArrow)) {
+				desired_velocity = new Vector3 (-1, 0, 0);
+				float temp;
+				if ((rb.position.y - grid_offset_y) % 0.5f < 0.25) {
+					temp = Mathf.Floor ((rb.position.y - grid_offset_y) / 0.5f) * 0.5f + grid_offset_y;
+				} else {
+					temp = Mathf.Ceil ((rb.position.y - grid_offset_y) / 0.5f) * 0.5f + grid_offset_y;
+				}
+				Vector3 newpos = new Vector3 (rb.transform.position.x, temp, 0);
+				rb.transform.position = newpos;
+			} else if (Input.GetKey (KeyCode.RightArrow)) {
+				desired_velocity = Vector3.right;
+				float temp;
+				if ((rb.position.y - grid_offset_y) % 0.5f < 0.25) {
+					temp = Mathf.Floor ((rb.position.y - grid_offset_y) / 0.5f) * 0.5f + grid_offset_y;
+				} else {
+					temp = Mathf.Ceil ((rb.position.y - grid_offset_y) / 0.5f) * 0.5f + grid_offset_y;
+				}
+				Vector3 newpos = new Vector3 (rb.transform.position.x, temp, 0);
+				rb.transform.position = newpos;
+			} else if (Input.GetKey (KeyCode.DownArrow)) {
+				desired_velocity = Vector3.down;
+				float temp;
+				if (rb.position.x % 0.5f < 0.25) {
+					temp = Mathf.Floor (rb.position.x / 0.5f) * 0.5f;
+				} else {
+					temp = Mathf.Ceil (rb.position.x / 0.5f) * 0.5f;
+				}
+				Vector3 newpos = new Vector3 (temp, rb.transform.position.y, 0);
+				rb.transform.position = newpos;
 			}
-			Vector3 newpos = new Vector3 (temp, rb.transform.position.y, 0);
-			rb.transform.position = newpos;
-		} else if (Input.GetKey (KeyCode.LeftArrow)) {
-			desired_velocity = new Vector3 (-1, 0, 0);
-			float temp;
-			if ((rb.position.y - grid_offset_y) % 0.5f < 0.25) {
-				temp = Mathf.Floor ((rb.position.y - grid_offset_y) / 0.5f) * 0.5f + grid_offset_y;
-			} else {
-				temp = Mathf.Ceil ((rb.position.y - grid_offset_y) / 0.5f) * 0.5f + grid_offset_y;
-			}
-			Vector3 newpos = new Vector3 (rb.transform.position.x, temp, 0);
-			rb.transform.position = newpos;
-		} else if (Input.GetKey (KeyCode.RightArrow)) {
-			desired_velocity = Vector3.right;
-			float temp;
-			if ((rb.position.y - grid_offset_y) % 0.5f < 0.25) {
-				temp = Mathf.Floor ((rb.position.y - grid_offset_y) / 0.5f) * 0.5f + grid_offset_y;
-			} else {
-				temp = Mathf.Ceil ((rb.position.y - grid_offset_y) / 0.5f) * 0.5f + grid_offset_y;
-			}
-			Vector3 newpos = new Vector3 (rb.transform.position.x, temp, 0);
-			rb.transform.position = newpos;
-		} else if (Input.GetKey (KeyCode.DownArrow)) {
-			desired_velocity = Vector3.down;
-			float temp;
-			if (rb.position.x % 0.5f < 0.25) {
-				temp = Mathf.Floor (rb.position.x / 0.5f) * 0.5f;
-			} else {
-				temp = Mathf.Ceil (rb.position.x / 0.5f) * 0.5f;
-			}
-			Vector3 newpos = new Vector3 (temp, rb.transform.position.y, 0);
-			rb.transform.position = newpos;
-		}
 
-        rb.velocity = desired_velocity * PlayerMovementVelocity;
+			rb.velocity = desired_velocity * PlayerMovementVelocity;
+		}
 
         /* NOTE:
          * A reminder to study and implement the grid-movement mechanic.
@@ -142,15 +179,97 @@ public class PlayerController : MonoBehaviour {
          */
     }
 
-    /* TODO: Deal with user-invoked usage of weapons and items */
-    void ProcessAttacks()
-    {
+	static public WeaponDefinition getWeaponDefinition(WeaponType wt) {
+		if (W_DEFS.ContainsKey (wt))
+			return W_DEFS [wt];
 
-    }
+		return (new WeaponDefinition ());
+	}
+
+    /* TODO: Deal with user-invoked usage of weapons and items */
+    void ProcessAttacks() {
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			current_state = EntityState.ATTACKING;
+			if (current_direction == Direction.SOUTH)
+				GetComponent<SpriteRenderer> ().sprite = link_attack [0];
+			else if (current_direction == Direction.WEST)
+				GetComponent<SpriteRenderer> ().sprite = link_attack [1];
+			else if (current_direction == Direction.NORTH)
+				GetComponent<SpriteRenderer> ().sprite = link_attack [2];
+			else //direction is EAST
+				GetComponent<SpriteRenderer> ().sprite = link_attack [3];
+
+			if (selected_weapon_prefab.name == "Sword") {
+				GameObject sword = GenerateWeapon (WeaponType.sword);
+				UseSword (sword, getWeaponDefinition(WeaponType.sword));
+			}
+		}
+	}
+
+	GameObject GenerateWeapon(WeaponType wt) {
+		GameObject go = Instantiate (selected_weapon_prefab) as GameObject;
+		WeaponDefinition def = getWeaponDefinition(wt);
+		go.tag = "Sword";
+		Vector3 LinkPos = rb.transform.position;
+//		Weapon w = new Weapon();
+//		w.SetType(wt);
+//		w.def = def;
+
+		switch(wt) {
+		case WeaponType.sword:
+			if (this.current_direction == Direction.SOUTH) {
+				go.GetComponent<SpriteRenderer> ().sprite = def.sprites_dlur [0];
+				LinkPos.x += 0.1f;
+				LinkPos.y -= 0.7f;
+				go.transform.position = LinkPos;
+			} else if (this.current_direction == Direction.WEST) {
+				go.GetComponent<SpriteRenderer> ().sprite = def.sprites_dlur [1];
+				LinkPos.x += 0.7f;
+				LinkPos.y -= 0.06f;
+				go.transform.position = LinkPos;
+			} else if (this.current_direction == Direction.NORTH) {
+				go.GetComponent<SpriteRenderer> ().sprite = def.sprites_dlur [2];
+				LinkPos.x -= 0.1f;
+				LinkPos.y += 0.7f;
+				go.transform.position = LinkPos;
+			} else { //current direction is EAST
+				go.GetComponent<SpriteRenderer> ().sprite = def.sprites_dlur [3];
+				LinkPos.x -= 0.7f;
+				LinkPos.y -= 0.06f;
+				go.transform.position = LinkPos;
+			}
+			break;
+		//more cases to come
+		}
+
+		return go;
+	}
+
+	void UseSword(GameObject sword, WeaponDefinition df) {
+		if (num_hearts >= 3) { //I'm not sure when he's allowed to shoot--it's either at 3 or "full capacity"
+			//it shoots
+			switch (this.current_direction) {
+			case Direction.SOUTH:
+				sword.GetComponent<Rigidbody> ().velocity = Vector3.down * df.velocity;
+				break;
+			case Direction.WEST:
+				sword.GetComponent<Rigidbody> ().velocity = Vector3.left * df.velocity;
+				break;
+			case Direction.NORTH:
+				sword.GetComponent<Rigidbody> ().velocity = Vector3.up * df.velocity;
+				break;
+			case Direction.EAST:
+				sword.GetComponent<Rigidbody> ().velocity = Vector3.right * df.velocity;
+				break;
+			}
+		} else {
+			//it just appears for a bit an then doesn't shoot
+		}
+	}
 
 	void OnCollisionEnter(Collision coll) {
-		print ("entered collision enter function");
-		if (coll.gameObject.tag == "Enemy") {
+		//print ("entered collision enter function");
+		if (coll.gameObject.tag == "Enemy" && num_cooldown_frames == 0) {
 			//receive_damage = true;
 			//do something here like decrease health? not sure how to put all
 			//the code together with these files, like difference between
@@ -160,6 +279,10 @@ public class PlayerController : MonoBehaviour {
 			//animation_state_machine.ChangeState(new StatePlayAnimationForDamage(this, GetComponent<SpriteRenderer>(),
 			ShowDamage(5);
 			num_hearts -= 0.5f;
+
+			num_cooldown_frames = 50;
+			GetComponent<Rigidbody> ().velocity *= (-1f * damage_hopback_vel);
+
 			if (num_hearts <= 0.0) {
 				print ("ah dude I ded");
 				//something idk...gotta start the dying animation I guess?
--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -82,6 +82,14 @@ public class StateIdleWithSprite : State
 	public override void OnStart()
 	{
 		renderer.sprite = sprite;
+		if (pc.link_run_down.Contains (sprite))
+			pc.current_direction = Direction.SOUTH;
+		else if (pc.link_run_left.Contains (sprite))
+			pc.current_direction = Direction.WEST;
+		else if (pc.link_run_up.Contains (sprite))
+			pc.current_direction = Direction.NORTH;
+		else if (pc.link_run_right.Contains (sprite))
+			pc.current_direction = Direction.EAST;
 	}
 	
 	public override void OnUpdate(float time_delta_fraction)
@@ -90,14 +98,16 @@ public class StateIdleWithSprite : State
 			return;
 
 		// Transition to walking animations on key press.
-		if(Input.GetKeyDown(KeyCode.DownArrow))
-			state_machine.ChangeState(new StatePlayAnimationForHeldKey(pc, renderer, pc.link_run_down, 6, KeyCode.DownArrow));
-		if(Input.GetKeyDown(KeyCode.UpArrow))
-			state_machine.ChangeState(new StatePlayAnimationForHeldKey(pc, renderer, pc.link_run_up, 6, KeyCode.UpArrow));
-		if(Input.GetKeyDown(KeyCode.RightArrow))
-			state_machine.ChangeState(new StatePlayAnimationForHeldKey(pc, renderer, pc.link_run_right, 6, KeyCode.RightArrow));
-		if(Input.GetKeyDown(KeyCode.LeftArrow))
-			state_machine.ChangeState(new StatePlayAnimationForHeldKey(pc, renderer, pc.link_run_left, 6, KeyCode.LeftArrow));
+		if (pc.num_cooldown_frames == 0) {
+			if (Input.GetKeyDown (KeyCode.DownArrow))
+				state_machine.ChangeState (new StatePlayAnimationForHeldKey (pc, renderer, pc.link_run_down, 6, KeyCode.DownArrow));
+			if (Input.GetKeyDown (KeyCode.UpArrow))
+				state_machine.ChangeState (new StatePlayAnimationForHeldKey (pc, renderer, pc.link_run_up, 6, KeyCode.UpArrow));
+			if (Input.GetKeyDown (KeyCode.RightArrow))
+				state_machine.ChangeState (new StatePlayAnimationForHeldKey (pc, renderer, pc.link_run_right, 6, KeyCode.RightArrow));
+			if (Input.GetKeyDown (KeyCode.LeftArrow))
+				state_machine.ChangeState (new StatePlayAnimationForHeldKey (pc, renderer, pc.link_run_left, 6, KeyCode.LeftArrow));
+		}
 	}
 }
 
@@ -132,6 +142,14 @@ public class StatePlayAnimationForHeldKey : State
 	public override void OnStart()
 	{
 		animation_start_time = Time.time;
+		if (key == KeyCode.DownArrow)
+			pc.current_direction = Direction.SOUTH;
+		else if (key == KeyCode.LeftArrow)
+			pc.current_direction = Direction.WEST;
+		else if (key == KeyCode.RightArrow)
+			pc.current_direction = Direction.EAST;
+		else if (key == KeyCode.UpArrow)
+			pc.current_direction = Direction.NORTH;
 	}
 	
 	public override void OnUpdate(float time_delta_fraction)
@@ -160,94 +178,94 @@ public class StatePlayAnimationForHeldKey : State
 			state_machine.ChangeState(new StatePlayAnimationForHeldKey(pc, renderer, pc.link_run_left, 6, KeyCode.LeftArrow));
 		
 		// If we detect the specified key has been released, return to the idle state.
-		else if(!Input.GetKey(key))
+		else if(!Input.GetKey(key) || pc.num_cooldown_frames > 0)
 			state_machine.ChangeState(new StateIdleWithSprite(pc, renderer, animation[1]));
 	}
 }
 
 // A State for playing an animation when damaged.
-public class StatePlayAnimationForDamage : State
-{
-	PlayerController pc;
-	SpriteRenderer renderer;
-	KeyCode key;
-	Sprite[] animation;
-	Sprite current;
-	int animation_length;
-	float animation_progression;
-	float animation_start_time = -1.0f; //this should be -1 if you want it set to the actual time
-	int fps;
-	float cooldown_time;
-	float total_damage_time;
-
-	public StatePlayAnimationForDamage(PlayerController pc, SpriteRenderer renderer, Sprite[] animation, int fps, float cooldown, float damage_time, KeyCode key, float start_time)
-	{
-		this.pc = pc;
-		this.renderer = renderer;
-		this.key = key;
-		this.animation = animation;
-		this.current = animation [0];
-		this.animation_length = animation.Length;
-		this.cooldown_time = cooldown;
-		this.total_damage_time = damage_time;
-		this.fps = fps;
-		if (start_time != -1.0f) {
-			this.animation_start_time = start_time;
-		}
-
-		if(this.animation_length <= 0)
-			Debug.LogError("Empty animation submitted to state machine!");
-	}
-
-	public override void OnStart()
-	{
-		//only set animation_start_time to actual time if none was given
-		if (animation_start_time != -1.0f)
-			animation_start_time = Time.time;
-	}
-
-	public override void OnUpdate(float time_delta_fraction)
-	{
-		if(pc.current_state == EntityState.ATTACKING)
-			return;
-
-		if(this.animation_length <= 0)
-		{
-			Debug.LogError("Empty animation submitted to state machine!");
-			return;
-		}
-
-		//this should stop the damage animation after "total damage time"
-		if (Time.time - animation_start_time > total_damage_time) {
-			if (pc.link_down_damage.Contains (current))
-				state_machine.ChangeState (new StateIdleWithSprite (pc, renderer, pc.link_run_down [1]));
-			else if (pc.link_left_damage.Contains (current))
-				state_machine.ChangeState (new StateIdleWithSprite (pc, renderer, pc.link_run_left [1]));
-			else if (pc.link_up_damage.Contains (current))
-				state_machine.ChangeState (new StateIdleWithSprite (pc, renderer, pc.link_run_up [1]));
-			else if (pc.link_right_damage.Contains (current))
-				state_machine.ChangeState (new StateIdleWithSprite (pc, renderer, pc.link_run_right [1]));
-		}
-
-		// Modulus is necessary so we don't overshoot the length of the animation.
-		int current_frame_index = ((int)((Time.time - animation_start_time) / (1.0 / fps)) % animation_length);
-		renderer.sprite = animation[current_frame_index];
-
-		if (Time.time - animation_start_time > cooldown_time) {
-			// If another key is pressed, we need to transition to a different walking animation.
-			if (Input.GetKeyDown (KeyCode.DownArrow))
-				state_machine.ChangeState (new StatePlayAnimationForDamage (pc, renderer, pc.link_down_damage, 6, cooldown_time, total_damage_time, KeyCode.DownArrow, animation_start_time));
-			else if (Input.GetKeyDown (KeyCode.UpArrow))
-				state_machine.ChangeState (new StatePlayAnimationForDamage (pc, renderer, pc.link_up_damage, 6, cooldown_time, total_damage_time, KeyCode.UpArrow, animation_start_time));
-			else if (Input.GetKeyDown (KeyCode.RightArrow))
-				state_machine.ChangeState (new StatePlayAnimationForDamage (pc, renderer, pc.link_right_damage, 6, cooldown_time, total_damage_time, KeyCode.RightArrow, animation_start_time));
-			else if (Input.GetKeyDown (KeyCode.LeftArrow))
-				state_machine.ChangeState (new StatePlayAnimationForDamage (pc, renderer, pc.link_left_damage, 6, cooldown_time, total_damage_time, KeyCode.LeftArrow, animation_start_time));
-			//eventually we can add in "static" flashing (so Link isn't "running" when
-			//there is no key down), but that's not super important right now
-		}
-	}
-}
+//public class StatePlayAnimationForDamage : State
+//{
+//	PlayerController pc;
+//	SpriteRenderer renderer;
+//	KeyCode key;
+//	Sprite[] animation;
+//	Sprite current;
+//	int animation_length;
+//	float animation_progression;
+//	float animation_start_time = -1.0f; //this should be -1 if you want it set to the actual time
+//	int fps;
+//	float cooldown_time;
+//	float total_damage_time;
+//
+//	public StatePlayAnimationForDamage(PlayerController pc, SpriteRenderer renderer, Sprite[] animation, int fps, float cooldown, float damage_time, KeyCode key, float start_time)
+//	{
+//		this.pc = pc;
+//		this.renderer = renderer;
+//		this.key = key;
+//		this.animation = animation;
+//		this.current = animation [0];
+//		this.animation_length = animation.Length;
+//		this.cooldown_time = cooldown;
+//		this.total_damage_time = damage_time;
+//		this.fps = fps;
+//		if (start_time != -1.0f) {
+//			this.animation_start_time = start_time;
+//		}
+//
+//		if(this.animation_length <= 0)
+//			Debug.LogError("Empty animation submitted to state machine!");
+//	}
+//
+//	public override void OnStart()
+//	{
+//		//only set animation_start_time to actual time if none was given
+//		if (animation_start_time != -1.0f)
+//			animation_start_time = Time.time;
+//	}
+//
+//	public override void OnUpdate(float time_delta_fraction)
+//	{
+//		if(pc.current_state == EntityState.ATTACKING)
+//			return;
+//
+//		if(this.animation_length <= 0)
+//		{
+//			Debug.LogError("Empty animation submitted to state machine!");
+//			return;
+//		}
+//
+//		//this should stop the damage animation after "total damage time"
+//		if (Time.time - animation_start_time > total_damage_time) {
+//			if (pc.link_down_damage.Contains (current))
+//				state_machine.ChangeState (new StateIdleWithSprite (pc, renderer, pc.link_run_down [1]));
+//			else if (pc.link_left_damage.Contains (current))
+//				state_machine.ChangeState (new StateIdleWithSprite (pc, renderer, pc.link_run_left [1]));
+//			else if (pc.link_up_damage.Contains (current))
+//				state_machine.ChangeState (new StateIdleWithSprite (pc, renderer, pc.link_run_up [1]));
+//			else if (pc.link_right_damage.Contains (current))
+//				state_machine.ChangeState (new StateIdleWithSprite (pc, renderer, pc.link_run_right [1]));
+//		}
+//
+//		// Modulus is necessary so we don't overshoot the length of the animation.
+//		int current_frame_index = ((int)((Time.time - animation_start_time) / (1.0 / fps)) % animation_length);
+//		renderer.sprite = animation[current_frame_index];
+//
+//		if (Time.time - animation_start_time > cooldown_time) {
+//			// If another key is pressed, we need to transition to a different walking animation.
+//			if (Input.GetKeyDown (KeyCode.DownArrow))
+//				state_machine.ChangeState (new StatePlayAnimationForDamage (pc, renderer, pc.link_down_damage, 6, cooldown_time, total_damage_time, KeyCode.DownArrow, animation_start_time));
+//			else if (Input.GetKeyDown (KeyCode.UpArrow))
+//				state_machine.ChangeState (new StatePlayAnimationForDamage (pc, renderer, pc.link_up_damage, 6, cooldown_time, total_damage_time, KeyCode.UpArrow, animation_start_time));
+//			else if (Input.GetKeyDown (KeyCode.RightArrow))
+//				state_machine.ChangeState (new StatePlayAnimationForDamage (pc, renderer, pc.link_right_damage, 6, cooldown_time, total_damage_time, KeyCode.RightArrow, animation_start_time));
+//			else if (Input.GetKeyDown (KeyCode.LeftArrow))
+//				state_machine.ChangeState (new StatePlayAnimationForDamage (pc, renderer, pc.link_left_damage, 6, cooldown_time, total_damage_time, KeyCode.LeftArrow, animation_start_time));
+//			//eventually we can add in "static" flashing (so Link isn't "running" when
+//			//there is no key down), but that's not super important right now
+//		}
+//	}
+//}
 
 // Additional recommended states:
 // StateDeath
