52bc6c65eed45bcefc0bae88337762a81fbc6140
Ori Lindner <olindner@umich.edu>
2017-01-30 18:38:25 -0500

    what is sleep?

--- Assets/Scripts/Goriya.cs
+++ Assets/Scripts/Goriya.cs
@@ -4,148 +4,219 @@ using UnityEngine;
 
 public class Goriya : MonoBehaviour {
 
-	public Sprite[] horizSprites;
+	public float speed;
+	public Sprite[] horizSprites; //defaults left
 	public Sprite[] upSprites;
 	public Sprite[] downSprites;
 	public float spriteDelay;
 	private float spriteTimer;
-	public int frames_between_throws;
+	public float throwDelay;
+	private float throwTimer;
+	private bool isMoving =  false;
+	Vector3 pos;
+	private int dir;
+	private bool isStunned = false;
+	private int here = 0;
 
 	public Room room;
 
 	public Direction current_direction;
-	public int frames_before_change = 0;
 
 	public GameObject boomerang;
 	public bool has_boomerang = true;
+	private SpriteRenderer sr;
 
-	public GoriyaBoomerang current_gb;
+	//public GoriyaBoomerang current_gb;
 
 	// Use this for initialization
 	void Start () {
 		spriteTimer = Time.time + spriteDelay;
-		frames_between_throws = Random.Range (50, 100);
+		throwTimer = Time.time + throwDelay;
+		pos = transform.position;
+		dir = Random.Range(0,3);
+		sr = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (frames_before_change == 0) {
-			frames_before_change = Random.Range (50, 100);
-			int temp = Random.Range (1, 4);
-			if (temp == 1) { //go up
-				GetComponent<SpriteRenderer> ().sprite = upSprites [0];
-				current_direction = Direction.NORTH;
-				GetComponent<Rigidbody> ().velocity = Vector3.up;
-			} else if (temp == 2) { //go right
-				GetComponent<SpriteRenderer> ().sprite = horizSprites [0];
-				GetComponent<SpriteRenderer> ().flipX = true;
-				current_direction = Direction.EAST;
-				GetComponent<Rigidbody> ().velocity = Vector3.right;
-			} else if (temp == 3) { //go down
-				GetComponent<SpriteRenderer> ().sprite = downSprites [0];
-				current_direction = Direction.SOUTH;
-				GetComponent<Rigidbody> ().velocity = Vector3.down;
-			} else if (temp == 4) { //go left
-				current_direction = Direction.WEST;
-				GetComponent<SpriteRenderer> ().sprite = horizSprites [0];
-				GetComponent<Rigidbody> ().velocity = Vector3.left;
-			}
-			spriteTimer = Time.time + spriteDelay;
-		} else {
-			frames_before_change--;
-			if (current_direction == Direction.NORTH) {
-				if (Time.time >= spriteTimer) {
-					if (GetComponent<SpriteRenderer> ().sprite != upSprites [0])
-						GetComponent<SpriteRenderer> ().sprite = upSprites [0];
-					else
-						GetComponent<SpriteRenderer> ().sprite = upSprites [1];
-					spriteTimer = Time.time + spriteDelay;
+
+		if (transform.position == pos)
+			isMoving = false;
+		else
+			isMoving = true;
+
+		if (!isMoving && !isStunned) {
+			int num = Random.Range (0, 15);
+
+			//Make sure set position in middle of square
+			CorrectPosition ();
+			////////////////////////////////////////////
+
+			Vector3 rayUp = transform.TransformDirection (Vector3.up);
+			Vector3 rayDown = transform.TransformDirection (Vector3.down);
+			Vector3 rayLeft = transform.TransformDirection (Vector3.left);
+			Vector3 rayRight = transform.TransformDirection (Vector3.right);
+			//RaycastHit hit;
+
+			//Equal percentages each direction...
+			if (num == 0 || num == 1) { //Up
+				if (!Physics.Raycast (transform.position, rayUp, 1)) {
+					pos += Vector3.up;
+					dir = 0;
 				}
-			} else if (current_direction == Direction.EAST) {
-				if (Time.time >= spriteTimer) {
-					if (GetComponent<SpriteRenderer> ().sprite != horizSprites [0])
-						GetComponent<SpriteRenderer> ().sprite = horizSprites [0];
-					else
-						GetComponent<SpriteRenderer> ().sprite = horizSprites [1];
-					GetComponent<SpriteRenderer> ().flipX = true;
-					spriteTimer = Time.time + spriteDelay;
+			} else if (num == 2 || num == 3) { //Down
+				if (!Physics.Raycast (transform.position, rayDown, 1)) {
+					pos += Vector3.down;
+					dir = 1;
 				}
-			} else if (current_direction == Direction.SOUTH) {
-				if (Time.time >= spriteTimer) {
-					if (GetComponent<SpriteRenderer> ().sprite != downSprites [0])
-						GetComponent<SpriteRenderer> ().sprite = downSprites [0];
-					else
-						GetComponent<SpriteRenderer> ().sprite = downSprites [1];
-					spriteTimer = Time.time + spriteDelay;
+			} else if (num == 4 || num == 5) { //Left
+				if (!Physics.Raycast (transform.position, rayLeft, 1)) {
+					pos += Vector3.left;
+					dir = 2;
 				}
-			} else { //direction.WEST
-				if (Time.time >= spriteTimer) {
-					if (GetComponent<SpriteRenderer> ().sprite != horizSprites [0])
-						GetComponent<SpriteRenderer> ().sprite = horizSprites [0];
-					else
-						GetComponent<SpriteRenderer> ().sprite = horizSprites [1];
-					spriteTimer = Time.time + spriteDelay;
+			} else if (num == 6 || num == 7) { //Right
+				if (!Physics.Raycast (transform.position, rayRight, 1)) {
+					pos += Vector3.right;
+					dir = 3;
 				}
+			} 
+			//...added likelihood to continue moving in same direction
+			//0=UP, 1=DOWN, 2=LEFT, 3=RIGHT
+			else if (num >= 8 && num <= 14) {
+				if (dir == 0 && !Physics.Raycast (transform.position, rayUp, 1))
+					pos += Vector3.up;
+				else if (dir == 1 && !Physics.Raycast (transform.position, rayDown, 1))
+					pos += Vector3.down;
+				else if (dir == 2 && !Physics.Raycast (transform.position, rayLeft, 1))
+					pos += Vector3.left;
+				else if (dir == 3 && !Physics.Raycast (transform.position, rayRight, 1))
+					pos += Vector3.right;
 			}
 		}
 
-		if (has_boomerang) {
-			if (frames_between_throws > 0) {
-				frames_between_throws--;
-			} else {
-				print ("time to throw boomerang for goriya");
-				ThrowBoomerang ();
+		if (Time.time >= spriteTimer) {
+			if (dir == 0 && here == 0) { //Up
+				sr.sprite = upSprites [here];
+				here = 1;
+			} else if (dir == 0 && here == 1) {
+				sr.sprite = upSprites [here];
+				here = 0;
+			} else if (dir == 1 && here == 0) { //Down
+				sr.sprite = downSprites [here];
+				here = 1;
+			} else if (dir == 1 && here == 1) {
+				sr.sprite = downSprites [here];
+				here = 0;
+			} else if (dir == 2 && here == 0) { //Left
+				sr.sprite = horizSprites [here];
+				sr.flipX = false;
+				here = 1;
+			} else if (dir == 2 && here == 1) {
+				sr.sprite = horizSprites [here];
+				sr.flipX = false;
+				here = 0;
+			} else if (dir == 3 && here == 0) { //Right
+				sr.sprite = horizSprites [here];
+				sr.flipX = true;
+				here = 1;
+			} else if (dir == 3 && here == 1) {
+				sr.sprite = horizSprites [here];
+				sr.flipX = true;
+				here = 0;
 			}
-		} else {
-			current_gb.goriya_pos = this.transform.position;
+			spriteTimer = Time.time + spriteDelay;
+		}
+
+		transform.position = Vector3.MoveTowards (transform.position, pos, Time.deltaTime * speed);
+
+		if (has_boomerang && Time.time >= throwTimer) {
+			//ThrowBoomerang ();
+			GameObject go = Instantiate (boomerang);
+			//go.GetComponent<SpriteRenderer>().gameObject.transform.position = transform.position;
+			go.gameObject.transform.position = transform.position;
+			print(go.gameObject.transform.position == transform.position);
+//			//Vector3 current_pos = transform.position;
+//			if (current_direction == Direction.NORTH) {
+//				go.GetComponent<GoriyaBoomerang>().current_direction = Direction.NORTH;
+//				go.GetComponent<GoriyaBoomerang>().target = new Vector3 (transform.position.x, transform.position.y + 4f, 0);
+//			} else if (current_direction == Direction.EAST) {
+//				go.GetComponent<GoriyaBoomerang>().current_direction = Direction.EAST;
+//				go.GetComponent<GoriyaBoomerang>().target = new Vector3 (transform.position.x + 4f, transform.position.y, 0);
+//			} else if (current_direction == Direction.SOUTH) {
+//				go.GetComponent<GoriyaBoomerang>().current_direction = Direction.SOUTH;
+//				go.GetComponent<GoriyaBoomerang>().target = new Vector3 (transform.position.x, transform.position.y - 4f, 0);
+//			} else { //current direction is WEST
+//				go.GetComponent<GoriyaBoomerang>().current_direction = Direction.WEST;
+//				go.GetComponent<GoriyaBoomerang>().target = new Vector3 (transform.position.x - 4f, transform.position.y, 0);
+//			}
+			has_boomerang = false;
 		}
 	}
 
 	void ThrowBoomerang() {
-		GameObject go = Instantiate (this.boomerang) as GameObject;
+		//print("b4");
+		//GameObject go = Instantiate (boomerang);
+		//print("after");
 //		if (go.GetComponent<GoriyaBoomerang> ().speed == 5f) {
 //			print ("WHHHHAFJFLFDKA");
 //		}
-		current_gb = new GoriyaBoomerang ();
-		current_gb.the_boomerang = go;
-		current_gb.goriya_pos = this.transform.position;
-		Vector3 current_pos = this.transform.position;
+//		current_gb = new GoriyaBoomerang ();
+//		current_gb.the_boomerang = go;
+//		current_gb.goriya_pos = this.transform.position;
+		//Vector3 current_pos = transform.position;
 		if (current_direction == Direction.NORTH) {
-			current_gb.current_direction = Direction.NORTH;
-			current_pos.y += 1;
-			current_gb.target = new Vector3 (current_pos.x, current_pos.y + 4, current_pos.z);
+			GameObject go = Instantiate (boomerang, new Vector3(transform.position.x, transform.position.y + 0.5f, 0), Quaternion.identity);
+			go.GetComponent<GoriyaBoomerang>().target = new Vector3 (transform.position.x, transform.position.y + 3.5f, 0);
 		} else if (current_direction == Direction.EAST) {
-			current_gb.current_direction = Direction.EAST;
-			current_pos.x += 1;
-			current_gb.target = new Vector3 (current_pos.x + 4, current_pos.y, current_pos.z);
+			GameObject go = Instantiate (boomerang, new Vector3(transform.position.x + 0.5f, transform.position.y, 0), Quaternion.identity);
+			go.GetComponent<GoriyaBoomerang>().target = new Vector3 (transform.position.x + 3.5f, transform.position.y, 0);
 		} else if (current_direction == Direction.SOUTH) {
-			current_gb.current_direction = Direction.SOUTH;
-			current_pos.y -= 1;
-			current_gb.target = new Vector3 (current_pos.x, current_pos.y - 4, current_pos.z);
+			GameObject go = Instantiate (boomerang, new Vector3(transform.position.x, transform.position.y - 0.5f, 0), Quaternion.identity);
+			go.GetComponent<GoriyaBoomerang>().target = new Vector3 (transform.position.x, transform.position.y - 3.5f, 0);
 		} else { //current direction is WEST
-			current_gb.current_direction = Direction.WEST;
-			current_pos.x -= 1;
-			current_gb.target = new Vector3 (current_pos.x - 4, current_pos.y, current_pos.z);
+			GameObject go = Instantiate (boomerang, new Vector3(transform.position.x - 0.5f, transform.position.y, 0), Quaternion.identity);
+			go.GetComponent<GoriyaBoomerang>().target = new Vector3 (transform.position.x - 3.5f, transform.position.y, 0);
 		}
-		go.transform.position = current_pos;
-		if (current_direction == Direction.NORTH) {
-			go.GetComponent<Rigidbody> ().velocity = Vector3.up * current_gb.speed;
-		} else if (current_direction == Direction.EAST) {
-			go.GetComponent<Rigidbody> ().velocity = Vector3.right * current_gb.speed;
-		} else if (current_direction == Direction.SOUTH) {
-			go.GetComponent<Rigidbody> ().velocity = Vector3.down * current_gb.speed;
-		} else { //curent direction is WEST
-			go.GetComponent<Rigidbody> ().velocity = Vector3.left * current_gb.speed;
-		}
+		//go.transform.position = current_pos;
+//		if (current_direction == Direction.NORTH) {
+//			go.GetComponent<Rigidbody> ().velocity = Vector3.up * current_gb.speed;
+//		} else if (current_direction == Direction.EAST) {
+//			go.GetComponent<Rigidbody> ().velocity = Vector3.right * current_gb.speed;
+//		} else if (current_direction == Direction.SOUTH) {
+//			go.GetComponent<Rigidbody> ().velocity = Vector3.down * current_gb.speed;
+//		} else { //curent direction is WEST
+//			go.GetComponent<Rigidbody> ().velocity = Vector3.left * current_gb.speed;
+//		}
 		has_boomerang = false;
 	}
 
-	void OnCollisionEnter(Collision coll) {
+	void OnCollisionEnter(Collision coll) { //caught it!
 		if (coll.gameObject.tag == "GoriyaBoomerang") {
 			Destroy (coll.gameObject);
 			has_boomerang = true;
-			frames_between_throws = Random.Range (50, 100);
+			throwTimer = Time.time + throwDelay;
 		}
+	}
+
+	void CorrectPosition ()
+	{
+		float tempx;
+		float tempy;
+		if (transform.position.x - Mathf.Floor (transform.position.x) <= 0.5) {
+			tempx = Mathf.Floor (transform.position.x);
+		}
+		else {
+			tempx = Mathf.Ceil(transform.position.x);
+		}
+
+		if (transform.position.y - Mathf.Floor (transform.position.y) <= 0.5) {
+			tempy = Mathf.Floor (transform.position.y);
+		}
+		else {
+			tempy = Mathf.Ceil(transform.position.y);
+		}
+		transform.position = new Vector3(tempx, tempy, 0);
+		isMoving = false; //hopefully make it pick new direction
 	}
 }
