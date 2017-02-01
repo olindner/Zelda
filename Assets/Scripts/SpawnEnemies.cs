8d3fda8038883eaa388a67e06a9efe6689c85b73
Ori Lindner <olindner@umich.edu>
2017-01-31 23:32:57 -0500

    fixed Goriya and half of wallmaster

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -26,10 +26,31 @@ public class SpawnEnemies : MonoBehaviour {
 
 			if (CameraPan.c.GetComponent<RoomController> ().active_col_index == 4 && CameraPan.c.GetComponent<RoomController> ().active_row_index == 2) {
 				if (playerx >= 65.5f && playerx <= 66.5f) { //player is on left side
-					if (playeryFloor >= 37f) { //above bottom 2 squares
+					if (playeryFloor >= 114f) { //above bottom 2 squares
 						spawn = new Vector3 (Mathf.Floor (playerx) - 1f, Mathf.Floor (playery) - 3f, 0);
 					} else {
 						spawn = new Vector3 (Mathf.Floor (playerx) - 1f, Mathf.Floor (playery) + 3f, 0);
+					}
+				}
+				else if (playerx >= 76.5f && playerx <= 77.5f) { //player is on right side
+					if (playeryFloor >= 114f) { //above bottom 2 squares
+						spawn = new Vector3 (Mathf.Floor (playerx) + 1f, Mathf.Floor (playery) - 3f, 0);
+					} else {
+						spawn = new Vector3 (Mathf.Floor (playerx) + 1f, Mathf.Floor (playery) + 3f, 0);
+					}
+				}
+				else if (playery >= 118f && playery <= 119.5f) { //player is on upper side
+					if (playerxFloor >= 68f) { //right 2 squares
+						spawn = new Vector3 (Mathf.Floor (playerx) - 3f, Mathf.Floor (playery) + 1f, 0);
+					} else {
+						spawn = new Vector3 (Mathf.Floor (playerx) + 3f, Mathf.Floor (playery) + 1f, 0);
+					}
+				}
+				else if (playery >= 112.5f && playery <= 113.5f) { //player is on lower side
+					if (playerxFloor >= 68f) { //right 2 squares
+						spawn = new Vector3 (Mathf.Floor (playerx) - 3f, Mathf.Floor (playery) - 1f, 0);
+					} else {
+						spawn = new Vector3 (Mathf.Floor (playerx) + 3f, Mathf.Floor (playery) - 1f, 0);
 					}
 				}
 
